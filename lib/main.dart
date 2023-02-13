92ff1f2e9b4a98f5efba3ea73fa6a849e7494b5d
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-14 00:53:05 +0900

    add hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  // const b = 'バナナ';
+  // const t = Text(b);
+  // const c = Center(child: t);
+  // const s = Scaffold(body: c);
+  // const a = MaterialApp(home: s);
+  // runApp(a);
+
+  const a = MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text('Hello World!!'),
+      ),
+    ),
+  );
+  runApp(a);
+}
